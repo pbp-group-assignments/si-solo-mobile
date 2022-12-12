bf41e7fcd160645d16e953d840a3c61121e6365f
mathildadellanova <mathilda.dellanova@ui.ac.id>
2022-12-12 18:49:07 +0700

    add changes on admin site

--- lib/admin/page/list_pengaduan.dart
+++ lib/admin/page/list_pengaduan.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:si_solo/drawer_admin.dart';
-// import 'package:si_solo/admin/util/fetch_pengaduan.dart';
+import 'package:si_solo/admin/util/fetch_list_pengaduan.dart';
 import 'package:getwidget/components/list_tile/gf_list_tile.dart';
 
-// class ListPengaduanPage extends StatefulWidget {
-//   const ListPengaduanPage({super.key, required this.index});
-//   final int index;
-  
-//   @override
-//   State<ListPengaduanPage> createState() => _ListPengaduanPage(index);
-// }
+class ListPengaduanPage extends StatefulWidget {
+  const ListPengaduanPage({super.key});
+
+  @override
+  State<ListPengaduanPage> createState() => _ListPengaduanPage();
+}
+
+class _ListPengaduanPage extends State<InfoWisataPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        title: const Text("List Pengaduan"),
+        ),
+        drawer: buildDrawer(context),
+        body: FutureBuilder(
+            future: fetchPengaduan(),
+            builder: (context, AsyncSnapshot snapshot) {
+                if (snapshot.data == null) {
+                    return const Center(child: CircularProgressIndicator());
+                } else {
+                    if (!snapshot.hasData) {
+                        return Column(
+                            children: [
+                            const Text("Belum ada pengaduan"),
+                            const SizedBox(height: 20,),
+                            ]
+                        );
+                    } else {
+                        return ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (_, index) => Container(
+                                Card(
+                                    child: Container(
+                                        padding: const EdgeInsets.symmetric(horizontal: 3, vertical: 7),
+                                        child: GFListTile(
+                                            titleText: '${snapshot.data![index].masalah}',
+                                            subTitleText: '${snapshot.data![index].deskripsiMasalah}',
+                                        ),
+                                    ),
+                                )
+                            )
+                        )
+                    }
+                }
+            }
+        )
+    )
+  }
+}
